43fc696bb09cf721d23600929199314fe4ac38f5
Sergio Nicolás <shaldaneg@unal.edu.co>
2023-09-21 21:34:46 -0500

    Update analizador.py

--- analizador.py
+++ analizador.py
@@ -40,14 +40,33 @@ def analizador_lexico(frase):
         'registro',
         'arreglo',
         'de',
-        'entonces'
+        'entonces',
+        'tkn_period',     
+        'tkn_comma',
+        'tkn_colon', 
+        'tkn_closing_bra',    
+        'tkn_opening_bra',   
+        'tkn_closing_par',   
+        'tkn_opening_par',
+        'tkn_plus',    
+        'tkn_minus',   
+        'tkn_times',   
+        'tkn_div',    
+        'tkn_power',    
+        'tkn_equal',    
+        'tkn_neq',     
+        'tkn_less',     
+        'tkn_leq',      
+        'tkn_greater',     
+        'tkn_geq', 
+        'tkn_assign',       
         }
     tokens = [
         ('tkn_real',   r'\d+(\.\d*)?'), 
         ('tkn_integer',   r'[0-9]'),           
         ('tkn_assign',   r'<-'),                      
         ('id',       r'[A-Za-z_0-9]+'),    
-        ('tkn_str',       r'["A-Za-z_0-9"]'),   
+        ('tkn_str',       r'["\w]+'),   
         ('tkn_period',       r'\.'),      
         ('tkn_comma',       r'\,'), 
         ('tkn_colon',       r'\:'), 
@@ -66,7 +85,7 @@ def analizador_lexico(frase):
         ('tkn_leq',       r'<='), 
         ('tkn_greater',       r'\>'), 
         ('tkn_geq',       r'>='), 
-        ('tkn_char',       r"['\w*']+"), 
+        ('tkn_char',       r"['\w']+"), 
         ('skip',     r'[ \t]+'), 
         ('newline',  r'\n'),
         ('NiF', r'.'),
@@ -81,9 +100,11 @@ def analizador_lexico(frase):
         token_value = mo.group()
 
         column = (mo.start()+1) - line_start
-        if(token_type == 'id' and token_value in palabrasReservadas):
+        if((token_type == 'id' and token_value in palabrasReservadas)):
             token_type=token_value
             print(f"<{token_value}, {line_num}, {column}>")
+        elif (token_type in palabrasReservadas):
+            print(f"<{token_type}, {line_num}, {column}>")
         elif token_type == 'newline':
             line_start = mo.end()
             line_num += 1
@@ -94,6 +115,9 @@ def analizador_lexico(frase):
             print(f'>>> Error lexico (linea: {line_num}, posicion: {column})')
             return None
         else:
+            if(token_type == 'tkn_char'):
+                print(f"<{token_type}, {token_value[1]}, {line_num}, {column}>")
+                continue
             print(f"<{token_type}, {token_value}, {line_num}, {column}>")
          
     
