10efee698cfaf7fe84e54dbb11d2466258772991
Sergio Nicolás <shaldaneg@unal.edu.co>
2023-09-25 07:27:09 -0500

    Update analizador.py

--- analizador.py
+++ analizador.py
@@ -12,59 +12,88 @@ def analizador_lexico(frase):
 
     palabras_reservadas = {'inicio', 'fin', 'entero', 'real', 'booleano', 'caracter', 'cadena', 'verdadero', 'falso', 'escriba', 'lea', 'llamar',
         'si', 'sino', 'y', 'o', 'mod', 'caso', 'mientras', 'haga', 'repita', 'hasta', 'para', 'procedimiento', 'var', 'retorne', 'funcion', 'nueva_linea',
-        'registro', 'arreglo', 'de', 'entonces',     
+        'registro', 'arreglo', 'de', 'entonces', 'div', 'fin si', 'fin caso', 'fin mientras','fin para', 'fin registro',
         }
     tokens = [
         ('one_line_comment', r'\/\/.*'),
-        ('multi_line_comment', r'\/\*([^*]|(\*+[^*/]))*\*+\/'), 
-        ('tkn_real', r'\d+(\.\d*)?'),    
-        ('tkn_assign', r'<-'),                      
-        ('id', r'[A-Za-z_0-9]+'),    
-        ('tkn_str', r'"([^"]*)"'),   
-        ('tkn_period', r'\.'),      
-        ('tkn_comma', r'\,'), 
-        ('tkn_colon', r'\:'), 
-        ('tkn_closing_bra', r'\]'), 
-        ('tkn_opening_bra', r'\['), 
-        ('tkn_closing_par', r'\)'), 
-        ('tkn_opening_par', r'\('), 
-        ('tkn_plus', r'\+'), 
-        ('tkn_minus', r'\-'), 
-        ('tkn_times', r'\*'), 
-        ('tkn_div', r'\/'), 
-        ('tkn_power', r'\^'), 
+        ('multi_line_comment', r'\/\*([^*]|(\*+[^*/]))*\*+\/'),
+        ('tkn_real', r'\d+\.\d+|\d+'),
+        ('tkn_assign', r'<-'),
+        ('close_fin', r'[Ff]in [Ss]i'),
+        ('close_caso', r'[Ff]in  [Cc]aso'),
+        ('close_mientras', r'[Ff]in  [Mm]ientras'),
+        ('close_para', r'[Ff]in [Pp]ara'),
+        ('close_registro', r'[Ff]in [Rr]egistro'),
+        ('id', r'[A-Za-z_0-9]+'),
+        ('tkn_str', r'"[^"]*"'),
+        ('tkn_period', r'\.'),
+        ('tkn_comma', r'\,'),
+        ('tkn_colon', r'\:'),
+        ('tkn_closing_bra', r'\]'),
+        ('tkn_opening_bra', r'\['),
+        ('tkn_closing_par', r'\)'),
+        ('tkn_opening_par', r'\('),
+        ('tkn_plus', r'\+'),
+        ('tkn_minus', r'\-'),
+        ('tkn_times', r'\*'),
+        ('tkn_div', r'\/'),
+        ('tkn_power', r'\^'),
         ('tkn_neq', r'<>'),
-        ('tkn_leq', r'<='),  
-        ('tkn_less', r'\<'), 
+        ('tkn_leq', r'<='),
+        ('tkn_less', r'\<'),
         ('tkn_geq', r'>='),
-        ('tkn_greater', r'\>'), 
-        ('tkn_equal', r'\='), 
-        ('tkn_char', r"'(.)'"), 
-        ('skip', r'[ \t]+'), 
+        ('tkn_greater', r'\>'),
+        ('tkn_equal', r'\='),
+        ('tkn_char', r"'(.)'"),
+        ('skip', r'[ \t]+'),
         ('newline', r'\n'),
         ('NiF', r'.'),
-        
-    ]
-    
-    generadorPatrones = '|'.join('(?P<%s>%s)' % pair for pair in tokens)
-    line_num = 1 
-    line_start = 0
 
-    for mo in re.finditer(generadorPatrones, frase):
+    ]
+
+    generadorPatrones = '|'.join('(?P<%s>%s)' % (pair[0], pair[1]) for pair in tokens)
+    line_num = 1
+    line_start = 0
+    iterator = re.finditer(generadorPatrones, frase)
+    a = True
+
+    for mo in iterator:
         token_type = mo.lastgroup
         token_value = mo.group()
-
         column = (mo.start()+1) - line_start
-        
-        if(token_type == 'id'):
+        if(token_type == 'id' or token_type == 'close_fin' or token_type == 'close_caso' or token_type == 'close_mientras' or token_type == 'close_para' or token_type == 'close_registro'):
             token_value = token_value.lower()
             if(token_value in palabras_reservadas):
-                token_type=token_value
-                print(f"<{token_value},{line_num},{column}>")
-                continue
+              print(f"<{token_value},{line_num},{column}>")
+              if(token_value=='fin' and column == 1):
+                 next_line_num = line_num
+                 next_line_start = 0
+                 while(a == True):
+                  try:
+                    next_token = next(iterator)
+                    next_token_type = next_token.lastgroup
+                    next_token_value = next_token.group()
+                    next_column = (next_token.start() + 1) - next_line_start
+                    if next_token_type == 'newline':
+                      next_line_start = next_token.end()
+                      next_line_num += 1
+                      continue
+                    elif next_token_type == 'skip':
+                      continue
+                    elif(next_token_type == 'one_line_comment' or next_token_type == 'multi_line_comment'):
+                      if(next_token_type == 'multi_line_comment'):
+                        next_line_num = next_line_num + next_token_value.count('\n')
+                      continue
+                    if not (next_token_type == 'newline' or next_token_type == 'skip' or token_type == 'one_line_comment' or token_type == 'multi_line_comment'):
+                      print(f'>>> Error lexico (linea: {next_line_num}, posicion: {next_column})')
+                      return None
+                  except StopIteration:
+                    a = False
+                    pass
+              continue
             print(f"<{token_type},{token_value},{line_num},{column}>")
             continue
-        
+
         elif token_type == 'newline':
             line_start = mo.end()
             line_num += 1
@@ -80,7 +109,10 @@ def analizador_lexico(frase):
                 continue
             if(token_type == 'tkn_str'):
                 n=len(token_value)
-                print(f"<{token_type},{token_value[1:(n-1)]},{line_num},{column}>")
+                if('\n' in token_value):
+                  print(f'>>> Error lexico (linea: {line_num}, posicion: {column})')
+                  return None
+                print(repr(f"<{token_type},{token_value[1:(n-1)]},{line_num},{column}>")[1:-1])
                 continue
             if(token_type == 'tkn_real'):
                 if('.' in token_value):
@@ -94,13 +126,13 @@ def analizador_lexico(frase):
                 continue
             print(f"<{token_type},{line_num},{column}>")
             continue
-        yield Token(token_type, token_value, line_num, column)  
+        yield Token(token_type, token_value, line_num, column)
 
 
 if __name__ == "__main__":
 
     s = sys.stdin.read()
-   
+
     for token in analizador_lexico(s):
         print(token)
-  
+
